3dbcfa17bf62e50591d4ccdcd9179b08feb433f1
Jozsef Molnar <molnarjozsef.j@gmail.com>
2024-06-28 03:01:48 +0200

    feat: add compose navigation

---
+++ composeApp/src/commonMain/kotlin/PinScreen.kt
@@ -0,0 +1,13 @@
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun PinScreen() {
+    Text(
+        text = "PIN screen",
+        modifier = Modifier.fillMaxSize()
+    )
+}
